d98419ff4164e334d4f7bab73ff2b81280ce2110
gb <gaurav.b@cirrius.com>
2016-01-21 19:33:17 +0530

    vg

--- app/src/main/java/inc/gb/gauravtest/MainActivity.java
+++ app/src/main/java/inc/gb/gauravtest/MainActivity.java
@@ -1,7 +1,12 @@
 package inc.gb.gauravtest;
 
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -9,6 +14,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,6 +22,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,16 +54,12 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager mViewPager;
 
     private DBHandler dbHandler;
-    public static String MYINTENT = "inc.gb.gauravtest";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
 
         dbHandler = new DBHandler(this);
         SQLiteDatabase db = dbHandler.getWritableDatabase();
@@ -59,7 +74,9 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-
+        long count = getRowCount(db);
+        if (count == 0)
+            new DemoAsync(MainActivity.this).execute("http://188.166.204.35/mobile-api/v3/schemes");
     }
 
 
@@ -127,6 +144,7 @@ public class MainActivity extends AppCompatActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        ListView brandListView;
         /**
          * The fragment argument representing the section number for this
          * fragment.
@@ -152,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            ListView brandListView = (ListView) rootView.findViewById(R.id.brandlist);
+            brandListView = (ListView) rootView.findViewById(R.id.brandlist);
             DBHandler handler = new DBHandler(getActivity());
 
             Cursor cursor = handler.getWritableDatabase().query(handler.TABLE_NAME, null, null, null, null, null, handler.SCHEME_DATE + " ASC");
@@ -169,7 +187,107 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    class DemoAsync extends AsyncTask<String, Integer, JSONObject> {
+        ProgressDialog dialog;
+        Context context;
 
+        public DemoAsync(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            dialog.setTitle("Loading....");
+            dialog.setMessage("Please Wait....");
+            dialog.show();
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... strings) {
+            String json;
+
+            JSONObject jsonObject = null;
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.setReadTimeout(10000);
+                connection.setConnectTimeout(15000);
+
+                connection.connect();
+                InputStream is = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "n");
+                }
+                is.close();
+                json = sb.toString();
+
+                try {
+                    jsonObject = new JSONObject(json);
+                } catch (JSONException e) {
+                    Log.e("JSON Parser", "Error parsing data " + e.toString());
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return jsonObject;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            super.onPostExecute(jsonObject);
+
+            try {
+                JSONObject newObject = jsonObject.getJSONObject("data");
+
+                for (Iterator<String> iter = newObject.keys(); iter.hasNext(); ) {
+                    String key = iter.next();
+                    JSONObject object = newObject.getJSONObject(key);
+                    ContentValues values = new ContentValues();
+                    for (Iterator<String> iterval = object.keys(); iterval.hasNext(); ) {
+                        String newkey = iterval.next();
+
+                        Object value = object.get(newkey);
+                        if (!object.isNull(newkey)) {
+                            if (newkey.contains("-"))
+                                newkey = newkey.replace("-", "_");
+
+                            if (value instanceof JSONArray) {
+                                JSONArray keyStr = (JSONArray) value;
+                                values.put(newkey, keyStr.toString());
+                            } else {
+                                String keyStr = (String) value;
+                                values.put(newkey, keyStr);
+                            }
+                        }
+                    }
+                    SQLiteDatabase db = dbHandler.getWritableDatabase();
+                    db.insert(DBHandler.TABLE_NAME, null, values);
+
+                    System.out.print("Object " + object);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            dialog.dismiss();
+        }
+
+    }
+
+    private long getRowCount(SQLiteDatabase mDatabase) {
+        String sql = "SELECT COUNT(*) FROM " + dbHandler.TABLE_NAME;
+        SQLiteStatement statement = mDatabase.compileStatement(sql);
+        long count = statement.simpleQueryForLong();
+        return count;
+    }
 
 
 }
